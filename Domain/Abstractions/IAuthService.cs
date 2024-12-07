272a2db886caf02eab45f1ed59c274d1a81d17f1
Thiago Moura <thiago.moura2000@hotmail.com>
2024-12-07 19:45:57 -0300

    feat: Implementação Token JWT
    - Geração do Token JWT

---
+++ Domain/Abstractions/IAuthService.cs
@@ -0,0 +1,4 @@
+﻿namespace Domain.Abstractions; 
+public interface IAuthService {
+    public string GenerateJWT(string email, string username);
+}
---
+++ Services/AuthService/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Services.AuthService;
+public class AuthService(IConfiguration configuration) : IAuthService {
+
+    private readonly IConfiguration _configuration = configuration;
+    public string GenerateJWT(string email, string username) {
+        var issuer = _configuration["JWT:Issuer"];
+        var audience = _configuration["JWT:Audience"];
+        var key = _configuration["JWT:Key"];
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new List<Claim> {
+            new("Email", email),
+            new("Username", username),
+            new("EmailIdentifier", email.Split("@").ToString()!),
+            new("CurrentTime", DateTime.Now.ToString())
+        };
+
+        _ = int.TryParse(_configuration["JWT:TokenExpirationDays"], out int tokenExpirationDays);
+
+        var token = new JwtSecurityToken
+            (
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(tokenExpirationDays),
+                signingCredentials: credentials
+            );
+
+        // O handler é responsável por manusear o token anteriormente gerado
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+}
